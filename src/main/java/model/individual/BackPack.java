828c3b386bcf4fdfac321f68240f028cda9acfcb
dqkhai4274 <43041967+dqkhai4274@users.noreply.github.com>
2021-06-02 13:58:08 +0700

    Update BackPack.java

--- src/main/java/model/individual/BackPack.java
+++ src/main/java/model/individual/BackPack.java
@@ -4,14 +4,13 @@ public class BackPack implements Individual {
 	private int numOfElement;
 	private double weight;
 	private Element[] elements = new Element[Element.MAX_ELEMENTS];
-	private Element[] poolElements;
+	private poolElements = poolElements.getElements();
 
 	public BackPack(Element[] poolElements){
 		// mỗi khi tạo 1 backpack mới, thì elements trong cái backpack này được chọn ngẫu nhiên
 		// lưu lại 10 items này vào 10 phần tử đầu của itemList for simple
 		// element nào k có trong backpack thì để weight = 0, số lượng elements có weight != 0 nên là 10
 		// nhớ tính toán weight của backpack và lưu lại
-		this.poolElements = poolElements;
 		for(int i = 0; i < Element.MAX_ELEMENTS/2; i++){
 			elements[i] = getNewRandomElement();
 			this.weight += elements[i].getWeight();
